a979be61861bba3f64e8276707e22cf545e18051
Sheikh A. Tahmid <sa2tahmi@uwaterloo.ca>
2022-06-19 17:23:33 -0400

    figured out how to scan and generate QR codes using 3rd party libraries, added temporary code to main activity to demonstrate it

--- android/app/src/main/java/com/example/quickbill/MainActivity.kt
+++ android/app/src/main/java/com/example/quickbill/MainActivity.kt
@@ -1,13 +1,16 @@
 package com.example.quickbill
 
+import android.content.Intent
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.quickbill.databinding.ActivityMainBinding
+import com.example.quickbill.util.*
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
@@ -28,5 +31,17 @@ class MainActivity : AppCompatActivity() {
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications))
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        // Temporary code for debugging the QR code scanner stuff. Will integrate properly when rest of app is ready.
+        startScan( this )
+        debugShowGeneratedPopupQrCode( this, "jimmy buckets" )
+    }
+
+    // Temporary code for debugging the QR code scanner stuff. Will integrate properly when rest of app is ready.
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data) // Ignore the fact that it's deprecated.
+        val scanResult = getScanResult( resultCode, data )
+        Log.d( "MainActivity - onActivityResult()", "Scan result: $scanResult" )
+        if ( scanResult != null ) debugPutScanResultInPopup( this, scanResult )
     }
 }
