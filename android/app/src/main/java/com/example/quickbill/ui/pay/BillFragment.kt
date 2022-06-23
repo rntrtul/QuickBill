d3b6814be3d5a0db7f1d4e84cefa5a75f43994ee
Fareed <sistthelord@gmail.com>
2022-06-23 02:23:50 -0400

    change qr screen to compose track selected items cost bill items title

--- android/app/src/main/java/com/example/quickbill/ui/pay/BillFragment.kt
+++ android/app/src/main/java/com/example/quickbill/ui/pay/BillFragment.kt
@@ -5,16 +5,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Checkbox
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.fragment.app.Fragment
 
 /**
@@ -29,16 +34,24 @@ class BillFragment : Fragment() {
         val locationId = requireArguments().get("location_id")
         val tableNumber = requireArguments().get("table_number")
 
+
         return ComposeView(requireContext()).apply {
             setContent {
-                Text(text = "table #$tableNumber at $locationId")
-                Box(
-                    Modifier
-                        .fillMaxWidth()
-                ) {
-                    Column {
-                        for (i in 1..5) {
-                            BillItem()
+                val orderList = remember { getOrderItems().toMutableList() }
+                val totalCost: MutableState<Double> = remember { mutableStateOf(0.0) }
+
+                Column {
+                    Text(text = "table #$tableNumber at restaurant $locationId")
+                    Text(text = "Pay $${totalCost.value}")
+                    Box(
+                        Modifier
+                            .fillMaxWidth()
+                    ) {
+                        BillList(
+                            orderList
+                        ) { item, selected ->
+                            if (selected) totalCost.value += item.cost
+                            else totalCost.value -= item.cost
                         }
                     }
                 }
@@ -47,10 +60,20 @@ class BillFragment : Fragment() {
     }
 }
 
-@Preview
+data class OrderItem(
+    val name: String,
+    val cost: Double,
+    val alreadyPayed: Boolean,
+)
+
 @Composable
-fun BillItem(name: String = "apple", cost: Double = 3.25) {
+fun BillItem(
+    item: OrderItem,
+    enabled: Boolean = true,
+    onSelectItem: (OrderItem, Boolean) -> Unit
+) {
     val isChecked = remember { mutableStateOf(false) }
+    val textColor: Color = if (enabled) Color.Black else Color.LightGray
 
     Row(
         verticalAlignment = Alignment.CenterVertically,
@@ -59,12 +82,48 @@ fun BillItem(name: String = "apple", cost: Double = 3.25) {
             .fillMaxWidth()
             .padding(8.dp)
     ) {
-        Checkbox(
-            checked = isChecked.value,
-            onCheckedChange = { isChecked.value = !isChecked.value })
-        Text(text = name)
-        Text(text = "$$cost")
-
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Checkbox(
+                checked = isChecked.value,
+                onCheckedChange = {
+                    isChecked.value = !isChecked.value
+                    onSelectItem(item, isChecked.value)
+                },
+                enabled = enabled
+            )
+            Text(
+                text = item.name,
+                color = textColor,
+                fontSize = 16.sp,
+            )
+        }
+        Text(
+            text = "$${item.cost}",
+            color = textColor,
+            fontWeight = FontWeight.Medium,
+            modifier = Modifier.padding(end = 8.dp),
+        )
     }
+}
 
+@Composable
+fun BillList(
+    list: List<OrderItem>, onSelectItem: (OrderItem, Boolean) -> Unit
+) {
+    Column(
+        Modifier.verticalScroll(
+            rememberScrollState()
+        )
+    ) {
+        for (item in list) {
+            BillItem(item, onSelectItem = onSelectItem)
+        }
+    }
+}
+
+private fun getOrderItems() = List(15) { i ->
+    OrderItem("$i", i.toDouble(), true)
 }
--- android/app/src/main/java/com/example/quickbill/ui/pay/PayFragment.kt
+++ android/app/src/main/java/com/example/quickbill/ui/pay/PayFragment.kt
@@ -4,54 +4,38 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.ui.platform.ComposeView
 import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
-import androidx.navigation.findNavController
+import androidx.navigation.Navigation
 import com.example.quickbill.R
-import com.example.quickbill.databinding.FragmentPayBinding
 
 class PayFragment : Fragment() {
-
-    private var _binding: FragmentPayBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val payViewModel =
-            ViewModelProvider(this).get(PayViewModel::class.java)
-
-        _binding = FragmentPayBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-
-        val textView: TextView = binding.textPay
-        payViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+        return ComposeView(requireContext()).apply {
+            setContent {
+                val view = this
+                Column {
+                    Text(text = "This is the pay fragment")
+                    // fixme: use inside code to transition to bill screen and populate bundle
+                    Button(
+                        onClick = {
+                            val bundle = bundleOf("location_id" to "deadbeef", "table_number" to 0)
+                            Navigation.findNavController(view)
+                                .navigate(R.id.action_navigation_pay_to_billFragment, bundle)
+                        }) {
+                        Text(text = "go to list")
+                    }
+                }
+            }
         }
-
-        // fixme: doesn't show up sometimes
-        val button = binding.goToBillList
-
-        // fixme: use inside code to transition to bill screen and populate bundle
-        button.setOnClickListener { view ->
-            val bundle = bundleOf("location_id" to "deadbeef", "table_number" to 0)
-            view.findNavController()
-                .navigate(R.id.action_navigation_pay_to_billFragment, bundle)
-
-        }
-
-        return root
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
     }
 }
