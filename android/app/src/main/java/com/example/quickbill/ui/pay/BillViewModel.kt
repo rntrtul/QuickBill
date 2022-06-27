595ef9617d106ba0c6250660d7bd6c9fd1cb1690
Tushar Sekhri <TusharSekhri@v1040-wn-rt-a-123-13.campus-dynamic.uwaterloo.ca>
2022-06-27 18:13:46 -0400

    API class with single instance enforced

--- android/app/src/main/java/com/example/quickbill/ui/pay/BillViewModel.kt
+++ android/app/src/main/java/com/example/quickbill/ui/pay/BillViewModel.kt
@@ -1,18 +1,14 @@
 package com.example.quickbill.ui.pay
 
+import com.example.quickbill.api.API
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.toMutableStateList
 import androidx.lifecycle.ViewModel
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import java.net.URL
-import com.google.gson.Gson
-import kotlinx.coroutines.runBlocking
 
 
 class BillViewModel : ViewModel() {
-    private val _bill: Bill = getBill()
+
+    private val _bill: Bill = API.instance.getBill()
 
     private val _items = _bill.lineItems.toMutableStateList()
     private val _totalCost = mutableStateOf(0.0)
@@ -31,13 +27,3 @@ class BillViewModel : ViewModel() {
 }
 
 
-private fun getBill(): Bill {
-    var result: String = ""
-    var job = GlobalScope.launch(Dispatchers.IO) {
-        result = URL("https://quickbill.alexnainer.com/api/order/location/L3GAERGV19EXB/table/1").readText()
-    }
-    runBlocking {
-        job.join() // wait until child coroutine completes
-    }
-    return Gson().fromJson(result, Bill::class.java)
-}
