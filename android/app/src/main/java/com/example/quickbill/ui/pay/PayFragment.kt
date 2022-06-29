ed86d82df94c0ae1d9f4c595d6ee625c7d73dac1
Fareed <sistthelord@gmail.com>
2022-06-29 01:38:34 -0400

    remove unneeded text in pay Fragment

--- android/app/src/main/java/com/example/quickbill/ui/pay/PayFragment.kt
+++ android/app/src/main/java/com/example/quickbill/ui/pay/PayFragment.kt
@@ -5,11 +5,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material.Button
 import androidx.compose.material.Text
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.platform.ComposeView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -28,12 +28,11 @@ class PayFragment : Fragment() {
         return ComposeView(requireContext()).apply {
             setContent {
                 val payViewModel: PayViewModel = viewModel()
-                val text by payViewModel.text.observeAsState()
 
-                val view = this
-                Column {
-                    text?.let { Text(text = it) }
-                    // fixme: use inside code to transition to bill screen and populate bundle
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
+                ) {
                     Button(
                         onClick = {
                             activity?.let { startScan(it) }
@@ -47,7 +46,7 @@ class PayFragment : Fragment() {
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
-        if ( API.instance.isQrCodeScanned() ) {
+        if (API.instance.isQrCodeScanned()) {
             API.instance.callBill() // Need to make call in case bill changes
             findNavController(this).navigate(R.id.action_navigation_pay_to_billFragment)
         }
