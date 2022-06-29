2f4434ec6584f64bc6ed438d038de6eee6ab030c
alexnainer <me@alexnainer.io>
2022-06-28 21:30:07 -0400

    convert string money to Money type

--- backend/src/routes/payment.ts
+++ backend/src/routes/payment.ts
@@ -1,29 +1,29 @@
 import express from "express";
 import { Request, Response } from "express";
-import { CreatePaymentRequest } from "square";
+import { CreatePaymentRequest, Money } from "square";
 
 import { paymentsApi } from "../api/square";
 
 const router = express.Router();
 
 router.post("/", async (req: Request, res: Response) => {
-    const { sourceId, idempotencyKey, amountMoney } = req.body;
+  const { sourceId, idempotencyKey, amountMoney } = req.body;
+  const money: Money = { amount: BigInt(amountMoney) };
 
-    const body: CreatePaymentRequest = {
-        sourceId,
-        idempotencyKey,
-        amountMoney,
-        autocomplete: false,
-    };
+  const body: CreatePaymentRequest = {
+    sourceId,
+    idempotencyKey,
+    amountMoney: money,
+    autocomplete: false,
+  };
 
-    try {
-        const { result, ...httpResponse } = await paymentsApi.createPayment(body);
-        res.status(httpResponse.statusCode).send(result.payment)
-
-    } catch (error) {
-        console.log(error);
-        res.sendStatus(500);
-    }
+  try {
+    const { result, ...httpResponse } = await paymentsApi.createPayment(body);
+    res.status(httpResponse.statusCode).send(result.payment);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
 });
 
 module.exports = router;
