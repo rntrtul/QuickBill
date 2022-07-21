7c0a409d1f16db63a030c8150071880df28a976b
Fareed <sistthelord@gmail.com>
2022-07-21 16:20:47 -0400

    make multiple quantity a dropdown like item can put more items in there now since the amount of space it takes up doesn't matter as much added arbitrary text input for an item

--- android/app/src/main/java/com/example/quickbill/ui/pay/BillFragment.kt
+++ android/app/src/main/java/com/example/quickbill/ui/pay/BillFragment.kt
@@ -1,17 +1,30 @@
 package com.example.quickbill.ui.pay
 
+import android.util.Log
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -19,6 +32,8 @@ import com.example.quickbill.R
 import com.example.quickbill.ui.theme.QuickBillTheme
 import com.example.quickbill.util.centsToDisplayedAmount
 import com.example.quickbill.util.getActivity
+import com.example.quickbill.util.parseMoney
+import com.example.quickbill.util.validateMoneyAmount
 import com.google.accompanist.swiperefresh.SwipeRefresh
 import com.google.accompanist.swiperefresh.rememberSwipeRefreshState
 import sqip.CardEntry
@@ -54,12 +69,15 @@ fun BillView(billViewModel: BillViewModel = viewModel()) {
                             onQuantityChange = { item, quantitySelected ->
                                 billViewModel.itemQuantityChosen(item, quantitySelected)
                             },
+                            onAmountPayingChange = { item, amount ->
+                                billViewModel.itemAmountPayingChange(item, amount)
+                            }
                         )
                     }
                     Text(
                         modifier = Modifier
                             .align(Alignment.End)
-                            .padding(8.dp),
+                            .padding(8.dp, end = 32.dp),
                         text = "Total: ${centsToDisplayedAmount(billViewModel.billTotal())}",
                         color = MaterialTheme.colorScheme.onBackground,
                         style = MaterialTheme.typography.titleMedium
@@ -105,6 +123,7 @@ fun BillList(
     list: List<BillItem>,
     onSelectItem: (BillItem, Boolean) -> Unit,
     onQuantityChange: (BillItem, Int) -> Unit,
+    onAmountPayingChange: (BillItem, Int) -> Unit,
 ) {
     Column {
         for (item in list) {
@@ -116,8 +135,10 @@ fun BillList(
                 itemQuantity = item.order.quantity,
                 quantitySelected = item.quantitySelected,
                 lineCost = item.order.totalMoney,
+                amountPaying = item.amountPaying,
                 onSelectItem = { checked -> onSelectItem(item, checked) },
-                onQuantityChange = { quantity -> onQuantityChange(item, quantity) }
+                onQuantityChange = { quantity -> onQuantityChange(item, quantity) },
+                onAmountPayingChange = { amount -> onAmountPayingChange(item, amount) },
             )
         }
     }
@@ -134,19 +155,22 @@ fun LineItem(
     itemQuantity: Int = 2,
     quantitySelected: Int = 1,
     lineCost: Money = Money(4010, "CAD"),
+    amountPaying: Money = Money(3500, "CAD"),
     onSelectItem: (Boolean) -> Unit = {},
-    onQuantityChange: (Int) -> Unit = {}
+    onQuantityChange: (Int) -> Unit = {},
+    onAmountPayingChange: (Int) -> Unit = {}
 ) {
-//    style = TextStyle(textDecoration = TextDecoration.LineThrough)
-    val textColor: Color =
-        if (itemAlreadyPaid) MaterialTheme.colorScheme.error else MaterialTheme.colorScheme.onBackground
+    var showDetail by remember { mutableStateOf(false) }
+    val crossedOut =
+        if (itemAlreadyPaid) TextStyle(textDecoration = TextDecoration.LineThrough) else null
+
     QuickBillTheme {
-        Column {
+        Column(modifier = Modifier.padding(bottom = 4.dp)) {
             Row(
                 verticalAlignment = Alignment.CenterVertically,
                 horizontalArrangement = Arrangement.SpaceBetween,
                 modifier = Modifier
-                    .padding(8.dp, bottom = 4.dp)
+                    .padding(horizontal = 8.dp)
                     .fillMaxWidth()
             ) {
                 Row(
@@ -160,30 +184,84 @@ fun LineItem(
                     )
                     Text(
                         text = itemName,
-                        color = textColor,
-                        style = MaterialTheme.typography.bodyLarge,
+                        style = MaterialTheme.typography.bodyLarge.merge(crossedOut),
                     )
                 }
-                Text(
-                    text = lineCost.displayAmount(),
-                    color = textColor,
-                    style = MaterialTheme.typography.bodyMedium,
-                    modifier = Modifier.padding(end = 8.dp)
-                )
-            }
-            if (itemQuantity > 1) {
                 Row(
                     verticalAlignment = Alignment.CenterVertically,
-                    modifier = Modifier
-                        .padding(start = 42.dp, bottom = 4.dp)
-                        .fillMaxWidth(),
-                    horizontalArrangement = Arrangement.Start
+                    horizontalArrangement = Arrangement.SpaceBetween
                 ) {
                     Text(
-                        text = "$itemQuantity x ${itemCost.displayAmount()}",
-                        color = textColor,
-                        style = MaterialTheme.typography.bodyLarge,
+                        text = amountPaying.displayAmount(),
+                        style = MaterialTheme.typography.bodyMedium.merge(crossedOut),
+                        modifier = Modifier.padding(end = 8.dp)
                     )
+                    Box(modifier = Modifier.width(32.dp)) {
+                        if (itemQuantity > 1) {
+                            val rotation: Float by animateFloatAsState(if (showDetail) 180f else 0f)
+
+                            IconButton(
+                                onClick = { showDetail = !showDetail },
+                                modifier = Modifier.rotate(rotation)
+                            ) {
+                                Icon(Icons.Filled.ArrowDropDown, "show detail for item")
+                            }
+                        }
+                    }
+                }
+            }
+            AnimatedVisibility(visible = showDetail) {
+                LineItemDetails(
+                    itemQuantity,
+                    lineCost,
+                    itemCost,
+                    amountPaying,
+                    quantitySelected,
+                    onQuantityChange,
+                    onAmountPayingChange
+                )
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun LineItemDetails(
+    itemQuantity: Int = 2,
+    lineCost: Money = Money(4010, "CAD"),
+    itemCost: Money = Money(2005, "CAD"),
+    amountPaying: Money = Money(3500, "CAD"),
+    quantitySelected: Int = 1,
+    onQuantityChange: (Int) -> Unit = {},
+    onAmountPayingChange: (Int) -> Unit = {}
+) {
+    // todo: add details on how much is paid for
+    var shownText by remember { mutableStateOf(amountPaying.displayAmount()) }
+    var isError by rememberSaveable { mutableStateOf(false) }
+
+
+    val focusManager = LocalFocusManager.current
+    QuickBillTheme {
+        Column(
+            modifier = Modifier.pointerInput(Unit) {
+                detectTapGestures(onTap = {
+                    focusManager.clearFocus()
+                })
+            }
+        ) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier
+                    .padding(start = 56.dp)
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Text(
+                    text = "${lineCost.displayAmount()} = $itemQuantity x ${itemCost.displayAmount()}",
+                    style = MaterialTheme.typography.bodyMedium,
+                )
+                Box(modifier = Modifier.padding(end = 32.dp)) {
                     QuantitySelector(
                         itemQuantity = itemQuantity,
                         quantitySelected = quantitySelected,
@@ -191,6 +269,30 @@ fun LineItem(
                     )
                 }
             }
+            TextField(
+                value = shownText,
+                onValueChange = {
+                    shownText = it
+                    isError = !validateMoneyAmount(shownText, lineCost.amount)
+                    if (!isError) {
+                        onAmountPayingChange(parseMoney(shownText))
+                        Log.d("Input", "$amountPaying")
+                    }
+                },
+                isError = isError,
+                label = { Text(text = "Amount Paying") },
+                singleLine = true,
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                keyboardActions = KeyboardActions(onDone = {
+                    if (!isError) {
+                        shownText = amountPaying.displayAmount()
+                    }
+                }),
+                modifier = Modifier
+                    .align(Alignment.End)
+                    .padding(end = 32.dp)
+                    .width(180.dp)
+            )
         }
     }
 }
@@ -229,54 +331,61 @@ fun PayBillButton(
 @Composable
 fun QuantitySelector(
     itemQuantity: Int = 2,
-    quantitySelected: Int = 1,
+    quantitySelected: Int = 100,
     onQuantityChange: (Int) -> Unit = {},
 ) {
     var expanded by remember { mutableStateOf(false) }
 
     QuickBillTheme {
-        Box(modifier = Modifier.padding(start = 32.dp)) {
-            ExposedDropdownMenuBox(
-                expanded = expanded,
-                onExpandedChange = { expanded = !expanded }
+        ExposedDropdownMenuBox(
+            expanded = expanded,
+            onExpandedChange = { expanded = !expanded }
+        ) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween,
             ) {
-                Row(
-                    verticalAlignment = Alignment.CenterVertically,
-                    horizontalArrangement = Arrangement.SpaceBetween,
-                ) {
-                    IconButton(onClick = {
+                IconButton(
+                    onClick = {
                         expanded = false
                         onQuantityChange(max(quantitySelected - 1, 1))
-                    }) {
-                        Icon(
-                            painter = painterResource(id = R.drawable.ic_baseline_remove_24),
-                            contentDescription = "remove item to pay for",
-                            tint = MaterialTheme.colorScheme.onBackground
-                        )
-                    }
-                    OutlinedButton(onClick = { expanded = true }) {
-                        Text(text = quantitySelected.toString())
-                    }
-                    IconButton(onClick = {
+                    },
+                    modifier = Modifier.width(22.dp)
+                ) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_baseline_remove_24),
+                        contentDescription = "remove item to pay for",
+                        tint = MaterialTheme.colorScheme.onBackground
+                    )
+                }
+                TextButton(
+                    onClick = { expanded = true },
+                    modifier = Modifier.width(54.dp)
+                ) {
+                    Text(text = quantitySelected.toString())
+                }
+                IconButton(
+                    onClick = {
                         expanded = false
                         onQuantityChange(min(quantitySelected + 1, itemQuantity))
-                    }) {
-                        Icon(Icons.Filled.Add, "Add item to pay for")
-                    }
-                }
-
-                ExposedDropdownMenu(
-                    expanded = expanded,
-                    onDismissRequest = { expanded = false }
+                    },
+                    modifier = Modifier.width(22.dp)
                 ) {
-                    for (i in 1..itemQuantity) {
-                        DropdownMenuItem(
-                            text = { Text(text = "$i") },
-                            onClick = {
-                                onQuantityChange(i)
-                                expanded = false
-                            })
-                    }
+                    Icon(Icons.Filled.Add, "Add item to pay for")
+                }
+            }
+
+            ExposedDropdownMenu(
+                expanded = expanded,
+                onDismissRequest = { expanded = false }
+            ) {
+                for (i in 1..itemQuantity) {
+                    DropdownMenuItem(
+                        text = { Text(text = "$i") },
+                        onClick = {
+                            onQuantityChange(i)
+                            expanded = false
+                        })
                 }
             }
         }
--- android/app/src/main/java/com/example/quickbill/util/Util.kt
+++ android/app/src/main/java/com/example/quickbill/util/Util.kt
@@ -2,6 +2,7 @@ package com.example.quickbill.util
 
 import android.content.Context
 import android.content.ContextWrapper
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 
 // Used for displaying prices and amount to pay.
@@ -11,6 +12,44 @@ fun centsToDisplayedAmount(amount: Int): String {
 
     return if (cents < 10) "$$dollars.0$cents" else "$$dollars.$cents"
 }
+
+fun parseMoney(s: String): Int {
+
+    val cleaned = s.replace("""[$,.]""".toRegex(), "")
+
+    if (cleaned.isEmpty()) {
+        Log.d("PARSE", cleaned)
+        return -1
+    }
+    var num = cleaned.toInt()
+
+    if (!s.contains('.')) {
+        num *= 100
+    }
+
+    return num
+}
+
+fun validateMoneyAmount(
+    s: String,
+    max: Int
+): Boolean {
+    // validates only 1 decimal and less than max and > 0
+    if (s.count { char -> char == '.' } > 1) {
+        Log.d("VALIDATE", "too many .")
+        return false
+    }
+
+    val num = parseMoney(s)
+
+    if (num > max || num < 0) {
+        Log.d("VALIDATE", "num ($num) out of range 0-$max")
+        return false
+    }
+
+    return true
+}
+
 
 fun Context.getActivity(): AppCompatActivity? = when (this) {
     is AppCompatActivity -> this
