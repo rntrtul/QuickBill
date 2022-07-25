70807f0d616c5ce3a95e5e0f8354d3821eb99d59
Alex Nainer <me@alexnainer.io>
2022-07-25 11:32:18 -0400

    adding payments

--- backend/src/routes/payment.ts
+++ backend/src/routes/payment.ts
@@ -3,13 +3,19 @@ import { Request, Response } from "express";
 import { CreatePaymentRequest, Money } from "square";
 import { UserOrder, PaymentBody } from "../types";
 
-import { paymentsApi } from "../api/square";
+import { paymentsApi, ordersApi } from "../api/square";
 import db from "../db";
 
 const router = express.Router();
 
 router.post("/", async (req: Request, res: Response) => {
-  const { sourceId, idempotencyKey, amountMoney, orderId, userOrder }: PaymentBody = req.body;
+  const {
+    sourceId,
+    idempotencyKey,
+    amountMoney,
+    orderId,
+    userOrder,
+  }: PaymentBody = req.body;
   const money: Money = { amount: BigInt(amountMoney), currency: "CAD" };
   console.log("money", money);
 
@@ -25,6 +31,18 @@ router.post("/", async (req: Request, res: Response) => {
     const { result, ...httpResponse } = await paymentsApi.createPayment(body);
     userOrder.paymentId = result.payment?.id;
     await db.order.addUserOrderToOrder(orderId, userOrder);
+
+    // const response = await ordersApi.retrieveOrder(orderId);
+    // const order = response.body;
+
+    const userOrders: UserOrder[] = await db.order.getUserOrdersByOrderId(
+      orderId
+    );
+
+    let total: bigint = BigInt(0);
+    for (const userOrder of userOrders) {
+      total += userOrder.amount?.amount || BigInt(0);
+    }
 
     console.log("result.payment", result.payment);
     res.status(httpResponse.statusCode).send(result.payment);
